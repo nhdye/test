7ded3c9a72af117ce6dc81a57c8e8c4c53ede1bf
nhdye <Nathan@Nathans-MBP.netgear.com>
2014-12-20 17:17:11 -0500

    added loop

--- testGit/main.cpp
+++ testGit/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    long long *array = new long long[100000000];
+    
+    for (long long i = 0; i < 100000000; ++i) {
+        cout << i << endl;
+    }
+    
+    
     return 0;
 }
