e98c7667dde9193b8f264cacbe1630fbd9647c8b
nhdye <Nathan@Nathans-MBP.netgear.com>
2014-12-20 17:04:40 -0500

    Initial Commit

---
+++ testGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testGit
+//
+//  Created by Nathan Dye on 12/20/14.
+//  Copyright (c) 2014 Nathan Dye. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
